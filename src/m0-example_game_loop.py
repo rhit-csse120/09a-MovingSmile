bcc950281c81bf45d2dc97699a8f477769f4882f
David Mutchler <mutchler@rose-hulman.edu>
2023-07-14 11:55:43 -0400

    Completed standalone project for Moving Smile PyGame tutorial.

---
+++ src/m0-example_game_loop.py
@@ -0,0 +1,19 @@
+import pygame
+import sys
+
+pygame.init()
+pygame.display.set_caption("Hello World")
+screen = pygame.display.set_mode((640, 480))
+
+while True:
+
+    for event in pygame.event.get():
+        print(event)  # Used for an example here
+        if event.type == pygame.QUIT:
+            sys.exit()
+        # Additional interactions
+
+    # Draw things on the screen
+    screen.fill((0, 0, 0))  # black
+
+    pygame.display.update()
